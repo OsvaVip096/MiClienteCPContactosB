fe9af68ae6ba89bf0859d21fb865f0787b09e32e
osvax <s15120062@alumnos.itsur.edu.mx>
2020-02-13 19:59:14 -0600

    Se implemento el CRUD del cliente Content Provider. Las ventanas esta diseñadas de la misma manera que MiBaseDatosP77B.

---
+++ app/src/main/java/com/osvaldovillalobosperez/miclientecpcontactosb/ContractCPContactos.java
@@ -0,0 +1,15 @@
+package com.osvaldovillalobosperez.miclientecpcontactosb;
+
+import android.net.Uri;
+
+public class ContractCPContactos {
+    public static final Uri CONTENT_URI = Uri.parse("content://com.osvaldovillalobosperez.mibasedatosp77b.provider/contactos");
+
+    public static final String FIELD_ID = "_id";
+    public static final String FIELD_USUARIO = "usuario";
+    public static final String FIELD_EMAIL = "email";
+    public static final String FIELD_TEL = "tel";
+    public static final String FIELD_FECHANACIMIENTO = "fecha_nacimiento";
+
+    public static final String[] PROJECTION = {"_id", "usuario", "email", "tel", "fecha_nacimiento"};
+}
